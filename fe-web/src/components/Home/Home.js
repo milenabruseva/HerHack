0853688e0f1f014324235016b002f21a9b135e8e
Milena Bruseva <milena.k.bruseva@gmail.com>
2021-06-27 00:57:30 +0300

    Update Home

--- fe-web/src/components/Home/Home.js
+++ fe-web/src/components/Home/Home.js
@@ -7,55 +7,37 @@ export default function Home() {
             <Container>
                 <h1>ToTheM00n Home 🚀🚀🚀🚀</h1>
                 <p>
-                    Angel investor innovator iteration success. Crowdsource
-                    android ownership seed round. Disruptive user experience
-                    early adopters virality burn rate crowdfunding.
-                    Bootstrapping gen-z business-to-business A/B testing.
-                    Strategy early adopters network effects iPad mass market
-                    creative user experience. Advisor creative technology assets
-                    infographic hackathon burn rate infrastructure scrum project
-                    twitter ownership vesting period gen-z. Stock seed round
-                    analytics iPad. Validation traction investor niche market
-                    learning curve ownership gen-z. Launch party facebook
-                    rockstar network effects iPhone lean startup venture.
-                    Android interaction design investor infrastructure channels
-                    advisor release long tail first mover advantage social media
-                    deployment startup success strategy.
+                We want to implement a financial platform for women  who want to keep track about the price of their preferred stocks in a watchlist in combination 
+with a Social Media Aggregator. Aggregating capital market related information posted on social media and creating a fundament to analyze can be a real gamechanger.
                 </p>
                 <h2>About Us 🚀🚀🚀🚀</h2>
-                <p>
-                    Angel investor innovator iteration success. Crowdsource
-                    android ownership seed round. Disruptive user experience
-                    early adopters virality burn rate crowdfunding.
-                    Bootstrapping gen-z business-to-business A/B testing.
-                    Strategy early adopters network effects iPad mass market
-                    creative user experience. Advisor creative technology assets
-                    infographic hackathon burn rate infrastructure scrum project
-                    twitter ownership vesting period gen-z. Stock seed round
-                    analytics iPad. Validation traction investor niche market
-                    learning curve ownership gen-z. Launch party facebook
-                    rockstar network effects iPhone lean startup venture.
-                    Android interaction design investor infrastructure channels
-                    advisor release long tail first mover advantage social media
-                    deployment startup success strategy.
-                </p>
+
+                <ul>
+                    <li><b>Astrid Varga</b>: Web Developer</li>
+                    <li><b>Barbara Treusch</b>: Reporting and Accounting Expert</li>
+                    <li><b>Canan Cengel</b>: Designer</li>
+                    <li><b>Gemma Vinolas</b>: Digital Product Manager</li>
+                    <li><b>Milena Bruseva</b>: Systems Architect and Backend Engineer</li>
+                </ul>
+
                 <h2>About Investing 🚀🚀🚀🚀</h2>
-                <p>
-                    Angel investor innovator iteration success. Crowdsource
-                    android ownership seed round. Disruptive user experience
-                    early adopters virality burn rate crowdfunding.
-                    Bootstrapping gen-z business-to-business A/B testing.
-                    Strategy early adopters network effects iPad mass market
-                    creative user experience. Advisor creative technology assets
-                    infographic hackathon burn rate infrastructure scrum project
-                    twitter ownership vesting period gen-z. Stock seed round
-                    analytics iPad. Validation traction investor niche market
-                    learning curve ownership gen-z. Launch party facebook
-                    rockstar network effects iPhone lean startup venture.
-                    Android interaction design investor infrastructure channels
-                    advisor release long tail first mover advantage social media
-                    deployment startup success strategy.
-                </p>
+                <h3>Equities/ Shares</h3>
+                <p>Shares are securities that represent shares in a stock corporation. The owners of a share - the shareholders - are therefore co-owners of the company; they make equity capital available to the company. The shareholders have membership rights, e.g. participation in the annual general meetings, information rights and entitlement to a share in profits (dividend).</p>
+                <h3>Bonds</h3>
+                <p>Bonds are also called fixed-income securities, bonds, notes, debentures or debentures. Banks, companies or the public sector issue them and thus procure debt capital, i.e. a loan, for a certain period of time. Those who buy bonds regularly receive interest and at the end of the bond's term the money invested is returned. About 27,000 bonds are traded.</p>
+                <h3>Investment Funds</h3>
+                <p>Funds are issued by fund companies. They bundle the money of many investors. The fund management manages the funds and, depending on the type of fund, invests them in equities (equity funds), bonds (bond funds) or other forms of investment. Investors who purchase a fund unit certificate thus invest simultaneously in various securities; this is often possible even with small euro amounts. The assets of the fund are deposited with a custodian bank. It is not part of the fund company's assets and is therefore called a special fund. In the event of insolvency of the fund company, the fund assets - and thus also the investors' money - are protected.</p>
+                <h3>ETFs, ETCs and ETNs</h3>
+                <p>ETF stands for Exchange Traded Fund. The term originates from the time when investment funds were not yet traded on the stock exchange.
+ETC stands for Exchange Traded Commodity. ETCs are exchange-traded debt securities whose value is linked to the development of one or more commodity prices. ETCs are not special funds. However, ETCs that can be traded in Frankfurt are usually collateralised. This means that the bond is covered by the stored commodity or a fiduciary agreement.
+ETN is the abbreviation for Exchange Traded Note. ETNs are exchange-traded bonds whose value is linked to the development of a market indicator. ETNs enable so-called exotic investments, e.g. in volatility (i.e. price fluctuations) or in currency pairs (e.g. US dollar/euro). Unlike ETFs, they are usually offered with a limited term. Like ETCs, ETNs are not special funds, but they are not collateralised.</p>
+                <h3>Cryptocurrency</h3>
+                <p>A cryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized networks based on blockchain technology—a distributed ledger enforced by a disparate network of computers. A defining feature of cryptocurrencies is that they are generally not issued by any central authority, rendering them theoretically immune to government interference or manipulation.</p>
+                <h3>Certificate</h3>
+                <p>A certificate securitises participation in the price development of particular securities or securities products.
+The holder of a certificate participates, for example, directly in the price development of an index (index certificate) or in that of a specially created basket of shares (basket certificate). Even though certificates are mostly based on equities or equity indices, from a legal standpoint they are considered bonds and do not in any way certify the right of ownership or shareholder privileges. The investor receives a law of contract from the issuer, to whom he temporarily lends his money.
+Certificates are freely tradable on each exchange trading day and can have limited or unlimited maturity. They do not entail regular earnings payouts and usually have a variable repayment amount. Their price development is coupled to that of the underlying security. Aside from equities and equity indices, currencies, interest rates and resources are other possible underlyings for certificates.
+Certificates are investment products. In contrast to leverage products, investment products enable the investor to participate one-to-one in the performance of the underlying security. Thus, these products are best suited for conservative investment strategies. Discount, basket and index certificates, as well as reverse convertibles belong to this category.</p>
             </Container>
         </main>
     )
