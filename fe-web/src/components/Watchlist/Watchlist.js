bb8e18dd7f53a07edf82e0f9f684be65fa46d2f6
Astrid Varga <astrid.varga@gmx.de>
2021-06-26 14:46:44 +0200

    fix margin on card list

--- fe-web/src/components/Watchlist/Watchlist.js
+++ fe-web/src/components/Watchlist/Watchlist.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
 import Card from '../Card'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginBottom: '2rem',
+    },
+}))
 
 const stocks = ['GME', 'TSLA', 'AAPL', 'AMC']
 
 export default function Watchlist() {
+    const classes = useStyles()
     return (
         <main className="watchlist">
             <Container>
                 <h1>ToTheM00n Watchlist 💎 👐 💎 👐 💎 👐 💎 👐💎</h1>
                 {stocks.map((stock, index) => (
-                    <article className="list-item" key={index}>
+                    <article className={classes.root} key={index}>
                         <Card stock={stock} />
                     </article>
                 ))}
