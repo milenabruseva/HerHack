c9c49d2f2f847ec0a827628c74069c2cf426aafe
Astrid Varga <astrid.varga@gmx.de>
2021-06-26 14:57:00 +0200

    set grey background color

--- fe-web/src/App.js
+++ fe-web/src/App.js
@@ -1,27 +1,39 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 import AppBar from './components/AppBar'
+import { Box, CssBaseline } from '@material-ui/core'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 
 import Home from './components/Home/Home'
 import Watchlist from './components/Watchlist/Watchlist'
 
 function App() {
+    const theme = createMuiTheme({
+        palette: {
+            background: {
+                default: '#f5f5f5',
+            },
+        },
+    })
     return (
-        <div className="App">
-            <header className="header">
-                <AppBar />
-            </header>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/watchlist">
-                        <Watchlist />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="App">
+                <header className="header">
+                    <AppBar />
+                </header>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/watchlist">
+                            <Watchlist />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </MuiThemeProvider>
     )
 }
 
